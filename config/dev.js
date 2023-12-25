bb14b8219d8f8ccf61f0deb7a6cbc50847061f3a
David Ben Ishai <david@kokoapps.co.il>
2023-12-25 11:43:42 +0200

    Changed dbUrl to env var

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,6 @@
+require('dotenv').config() // for using .env file
 module.exports = {
   // dbURL:'mongodb+srv://David:db2eatdb@cluster0.txsv9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-  dbURL:'mongodb+srv://benditi:cat34car%21@cluster0.imtdz.mongodb.net/trellops?retryWrites=true&w=majority'
+  dbURL:process.env.DB_URL
 };
 
